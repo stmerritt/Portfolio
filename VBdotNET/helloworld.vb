e578590d996d04295f170682ffb5e0f5ea2d04e7
Stephen Merritt <75268745+stmerritt@users.noreply.github.com>
2021-10-26 13:29:56 -0400

    Add files via upload

---
+++ VBdotNET/helloworld.vb
@@ -0,0 +1,10 @@
+'This code is written by Stephen Merritt for his portfolio
+
+Imports System
+Module Module1
+   Sub Main()
+      Console.WriteLine("Hello World!")
+	  Console.WriteLine("This is Stephen!")
+      Console.ReadKey()
+   End Sub
+End Module
