a72a72109eb8e5ad003313538bc1956b07134672
Stephen Merritt <75268745+stmerritt@users.noreply.github.com>
2021-10-26 13:02:51 -0400

    Add files via upload
    Adding helloworld for Java

---
+++ Java/helloworld.java
@@ -0,0 +1,9 @@
+/*This code is written by Stephen Merritt for his portfolio*/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("This is Stephen!");
+	}
+}
