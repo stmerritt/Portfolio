74af9cdd765e5007dfd2ee938b8f1826af396f52
Stephen Merritt <stmerritt19@gmail.com>
2021-10-27 20:15:10 -0400

    Update copyspecial.py
    my solution to the problem

--- Python/google-python-exercises/copyspecial/copyspecial.py
+++ Python/google-python-exercises/copyspecial/copyspecial.py
@@ -10,15 +10,28 @@ import sys
 import re
 import os
 import shutil
-import commands
+# import commands
+
+from datetime import datetime
 
 """Copy Special exercise
 """
 
 # +++your code here+++
 # Write functions and modify main() to call them
+def copyToDir(todir, dirs):
+  if todir != '':
+    for dir in dirs:
+      shutil.copy(dir, todir)
 
-
+def copyToZip(tozip, dirs, dt_string):
+  if tozip != '':
+    i = 0
+    for dir in dirs:
+      dir_name = 'zip_dir_' + str(i) + dt_string
+      i = i + 1
+      shutil.make_archive(dir_name, 'zip', dir)
+      shutil.move(dir + '/' + dir_name, tozip)
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -26,9 +39,12 @@ def main():
 
   # Make a list of command line arguments, omitting the [0] element
   # which is the script itself.
+  now = datetime.now()
+  dt_string = now.strftime("%d_%m_%Y__%H:%M:%S")
+
   args = sys.argv[1:]
   if not args:
-    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+    print("usage: [--todir dir][--tozip zipfile] dir [dir ...]")
     sys.exit(1)
 
   # todir and tozip are either set from command line
@@ -45,11 +61,14 @@ def main():
     del args[0:2]
 
   if len(args) == 0:
-    print "error: must specify one or more dirs"
+    print("error: must specify one or more dirs")
     sys.exit(1)
 
   # +++your code here+++
   # Call your functions
+  copyToDir(todir, args)
+
+  copyToZip(tozip, args, dt_string)
   
 if __name__ == "__main__":
   main()
